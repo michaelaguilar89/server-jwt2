b5914c80ca04d3fce38bdcd6f2dbe61b036b955a
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2023-10-27 09:47:29 -0600

    adding model, mongodb.driver

---
+++ server-Jwt2/Models/client.cs
@@ -0,0 +1,18 @@
+﻿namespace server_Jwt2.Models
+{
+    public class client
+    {
+        public string? Id { get; set; }
+        public string? firstName { get; set; }
+
+        public string? lastName { get; set; }
+
+        public int? age { get; set; }
+
+        public decimal? salary { get; set; }
+
+        public string? country { get; set; }
+
+        public string? photo { get; set; }
+    }
+}
