fcea2c756dfd65c5da5c84a6b107de977ec4deaf
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2023-10-31 12:59:24 -0600

    adding user Controller

---
+++ server-Jwt2/userController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server_Jwt2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class userController : ControllerBase
+    {
+    }
+}
