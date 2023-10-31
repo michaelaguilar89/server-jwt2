2c6e0a063d971d4a7701f1fca6695ba5346893d8
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2023-10-31 12:58:01 -0600

    adding userService,crud Operations

---
+++ server-Jwt2/Models/user.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace server_Jwt2.Models
+{
+    public class user
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonRequired]
+        public string? userName { get; set; }
+        [BsonRequired]
+        public string? password { get; set; }
+        [BsonRequired]
+        public string? photo { get; set; }
+    }
+}
---
+++ server-Jwt2/Repository's/userService.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using server_Jwt2.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace server_Jwt2.Repository_s
+{
+    public class userService
+    {
+        private readonly IMongoCollection<user> _userCollection;
+
+        public userService(IOptions<userDataBaseSettings> userDataBaseSettings)
+        {
+            
+            {
+                var mongoClient = new MongoClient(
+                    userDataBaseSettings.Value.ConnectionString);
+
+                var mongoDatabase = mongoClient.GetDatabase(
+                    userDataBaseSettings.Value.DatabaseName);
+
+                _userCollection = mongoDatabase.GetCollection<user>(
+                    userDataBaseSettings.Value.UsersCollectionName);
+
+                
+            }
+        }
+
+       public async Task<List<user?>> GetUser()
+        {
+                var users = await _userCollection.Find(_ => true).ToListAsync();
+                if (users!=null)
+                {
+                    return users;
+                }
+                return null;
+                        
+        }
+        public async Task<string> createUser(user myUser)
+        {
+            try
+            {
+                 
+
+                var userData= await _userCollection.Find(x=>x.userName==myUser.userName).ToListAsync();
+                if (userData!=null)
+                {
+                    return "userName is taken!";
+                }
+                await _userCollection.InsertOneAsync(myUser);
+                return "true";
+            }
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+        }//end of create user
+        public async Task<Object> getUserDetails(user myUser)
+        {
+            Object resp = new object();
+            try
+            {
+                var result = await _userCollection
+                    .Find(x => x.userName == myUser.userName && 
+                      x.password == myUser.password).FirstOrDefaultAsync();
+                if (result !=null)
+                {
+                   return result;
+                }
+                return false;
+
+            }
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+        }//end of getUsersDetails
+        public async Task<string> updateUser(user myUser)
+        {
+            try
+            {
+                var result = await _userCollection.Find(x => x.Id == myUser.Id).FirstOrDefaultAsync();
+                if (result!=null)
+                {
+                    await _userCollection.ReplaceOneAsync(x=>x.Id == myUser.Id,myUser);
+                    return "user has been update";
+                }
+                return "User invalid or not found";
+            }
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+        }//end of update
+        public async Task<string> removeUser(string id)
+        {
+            try
+            {
+                var result = await _userCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+                if (result != null)
+                {
+                    await _userCollection.DeleteOneAsync(x=>x.Id==id);
+                        return "user has been removed";
+                }
+                return "User invalid or not found";
+            }
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+        }//end of remove
+
+
+    }
+}
